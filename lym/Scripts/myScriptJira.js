83ddaee9c964b764e67e9d8e1e4ade00af51e69c
Yiming Liu <yiming.sh.liu@partner.bmw.com>
2017-07-21 09:52:57 +0800

    jira 点击按钮触发

--- lym/Scripts/myScriptJira.js
+++ lym/Scripts/myScriptJira.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         JiraModule
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  try to take over the world!
 // @author       You
 // @match        http://suus0002.w10:8080/browse/*
@@ -16,11 +16,12 @@
             $('#customfield_11200').val('11305');
             $('#timetracking_originalestimate').val('1d');
             $('#timetracking_remainingestimate').val('1d');
-            $('#qf-create-another').prop('checked', true);
+            //$('#qf-create-another').prop('checked', true);
         } else {
             setTimeout(m, 100);
         }
     };
+    $('#create-subtask').click(function () { setTimeout(setModule, 100); });
     onkeydown = function (e) {
         if (e.key == 'b' && (e.target.tagName == 'BODY' || e.target.tagName == 'A')) {
             if (!$('#summary').length) {
