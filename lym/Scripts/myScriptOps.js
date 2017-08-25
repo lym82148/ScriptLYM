7d67bfbe1376f73ca8f25192622666d8fea61448
Yiming Liu <yiming.sh.liu@partner.bmw.com>
2017-08-25 16:24:21 +0800

    auto email config

--- lym/Scripts/myScriptOps.js
+++ lym/Scripts/myScriptOps.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         OpsAutoEmail
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  try to take over the world!
 // @author       You
 // @match        https://omcops.bmw.com.cn/Operation/Release/ReleasePlanDetails/*
@@ -16,10 +16,8 @@
         return;
     }
     $('body').append('<style>.hideForCopy{display:none;}</style>');
+    var div = document.createElement('div');
     var cFun = function (tmp) {
-        if (div === undefined) {
-            div = document.createElement('div');
-        }
         div.innerHTML = tmp;
         $('body').children().addClass('hideForCopy');
         document.body.appendChild(div);
@@ -30,6 +28,7 @@
         $('body').children().removeClass('hideForCopy');
         return true;
     };
+    var configRecord = '无';
     var copy = function () {
         var data = {};
         data.serviceName = $("input#Service").val() || '';
@@ -39,33 +38,96 @@
         //releaseNote=releaseNote.split('\n').map(function(a,b){if(a){return a.trim();}}).filter(function(a){return a;}).join('\r\n');
         data.tagName = $('input#GitRepoRefTag').val();
         data.url = location.href;
+        data.config = configRecord;
+        var copyModel = model;
         for (var a in data) {
-            model = model.replace('{{' + a + '}}', data[a]);
+            copyModel = copyModel.replace('{{' + a + '}}', data[a]);
         }
-        cFun(model);
-        var x = $(this).hide(1000);
-        function st() { x.show(); }
-        setTimeout(st, 2000);
+        cFun(copyModel);
     };
     var serviceName = $("input#Service").val() || '';
     var releaseEnv = $("input#ReleaseEnv").val() || '';
     var title = 'deploy ' + serviceName + ' to ' + releaseEnv;
     var cc = 'DL-bmwconnected-bumper <bmwconnected-bumper@list.bmw.com>';
-    var mailto = 'Wang Bonnie, US-CN <Bonnie.Wang@bmw.com>; Shao Zach, US-CN <Zach.Shao@bmw.com>';
-    var content = '';
-
-    content = 'Hi, bonnie & zach\r\n\r\n' + serviceName + ' 申请发' + releaseEnv + ' 环境，需要ops上approve。\r\n\r\n\r\n\r\n';
+    var mailto = 'Wang Bonnie, US-CN <Bonnie.Wang@bmw.com>; Shao Zach, US-CN <Zach.Shao@bmw.com>; ';
+    var omc = 'omc.cn.support <omc.cn.support@bmwgroup.com>; ';
+    var content = 'Hi, bonnie & zach\r\n\r\n' + serviceName + ' 申请发' + releaseEnv + ' 环境，需要ops上approve。\r\n\r\n\r\n\r\n';
+    var omcContent = 'Hi, bonnie & zach & omc team \r\n\r\n' + serviceName + ' 申请发' + releaseEnv + ' 环境，需要ops上approve。\r\n\r\n\r\n\r\n';
 
     //mailto = 'Liu Yiming, (Yiming.SH.Liu@partner.bmw.com)';
     //cc = mailto;
 
     mailto = encodeURIComponent(mailto);
+    omc = encodeURIComponent(omc);
     content = encodeURIComponent(content);
+    omcContent = encodeURIComponent(omcContent);
     title = encodeURIComponent(title);
     cc = encodeURIComponent(cc);
-    $('h1').append('<a style="color:red;text-decoration:underline" href="mailto:' + mailto + '&cc=' + cc + '&subject=' + title + '&body=' + content + '"> Click Here To Email </a>').find('a').click(copy);
+    var mailTo = document.createElement('a');
+    mailTo.href = 'mailto:';
+    mailTo.style.display = 'none';
+    $('h1').append(mailTo);
 
-    var div;
+    var link = document.createElement('a');
+    link.style.color = 'red';
+    link.style.textDecoration = 'underline';
+    link.innerHTML = ' Click Here To Email ';
+    link.onclick = function () {
+        if ($(this).html() != ' Click Here To Email ') { return; }
+        $(this).html('Getting Config...');
+        $(this).css('color', 'pink');
+        var hasConfig;
+        var that = this;
+        $.ajax('https://omcops.bmw.com.cn/Configuration/DeployConfiguration/Index/' + releaseEnv + '-All').then(function (data) {
+            $(that).css('color', 'red');
+            $(that).html(' Click Here To Email ');
+            var arr = $(data).find('table tr').map(function (a, b) { if (b.firstElementChild.innerText.trim() == serviceName) return b; });
+            var configContent;
+            for (var i = 0; i < arr.length; i++) {
+                if (arr[i].cells[4].innerText.trim() == 'WaitAllows') {
+                    var date = new Date(arr[i].cells[5].innerText.trim());
+                    var dateMin = new Date(+new Date() - 86400000 * 3);
+                    if (date > dateMin) {
+                        var cells = arr[i].cells;
+                        configRecord = (cells[1].innerText + cells[4].innerText + cells[5].innerText).trim().replace(/\s{2,100}/g, '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
+                        configRecord = '<span style="color:#ff4nnnnnnnnnne4e">' + configRecord + '</span>';
+                        configContent = arr[i].getAttribute('data-cache');
+                        break;
+                    }
+                }
+            }
+            if (!!configContent) {
+                confirmBox('' + configContent);
+            } else {
+                confirmCopy("noconfig");
+            }
+        });
+        copy();
+    };
+    div2 = '<div class="modal fade" data-show="true"><div class="modal-dialog" style="left:0px"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">确认是否包含配置</h4></div><div class="modal-body">' + '' + '</div><div style="padding-left:25%;padding-right:25%;padding-bottom:20px"><button type="button" class="btn btn-primary" data-dismiss="modal" aria-hidden="true" style="width:100px" data-res="yes">是</button><button type="button" data-res="no"  class="pull-right btn btn-danger" data-dismiss="modal"style="width:100px" aria-hidden="true">否</button></div></div></div></div>';
+    div2 = $(div2);
+    var confirmCopy = function (type) {
+        $('.modal-backdrop').remove();
+        div2.remove();
+        div2.find('button').click(confirmCopy);
+        if (type == "noconfig" || $(this).data('res') == 'no') {
+            configRecord = '无';
+            mailTo.href = 'mailto:' + mailto + '&cc=' + cc + '&subject=' + title + '&body=' + content;
+        }
+        else {
+            mailTo.href = 'mailto:' + mailto + omc + '&cc=' + cc + '&subject=' + title + '&body=' + omcContent;
+        }
+        copy();
+        mailTo.click();
+    };
+    div2.find('button').click(confirmCopy);
+    var confirmBox = function (content) {
+        div2.find('.modal-body').html(content);
+        div2.modal('show');
+    };
+    $('h1').append(link);
+    //$('h1').append('<a style="color:red;text-decoration:underline" href="mailto:' + mailto + '&cc=' + cc + '&subject=' + title + '&body=' + content + '"> Click Here To Email </a>').find('a').click(copy);
+
     /*jshint multistr:true */
     var model = '<div>\
 \
@@ -132,7 +194,7 @@ border-top:none;background:#9BC2E6;padding:0cm 5.4pt 0cm 5.4pt;height:7.1pt">\
 <td width="831" valign="bottom" style="width:623.25pt;border-top:none;border-left:\
 none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;\
 padding:0cm 5.4pt 0cm 5.4pt;height:7.1pt">\
-<p class="MsoNormal"><span style="font-family:宋体;color:#1F497D">无</span><span lang="EN-US"><o:p></o:p></span></p>\
+<p class="MsoNormal"><span style="font-family:宋体;color:#1F497D">{{config}}</span><span lang="EN-US"><o:p></o:p></span></p>\
 </td>\
 </tr>\
 <tr style="mso-yfti-irow:6;height:17.9pt">\
