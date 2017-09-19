36fdc313ac4237500cf182c7714d1dd05d05cd30
Yiming Liu <yiming.sh.liu@partner.bmw.com>
2017-09-19 13:23:25 +0800

    fix version

--- lym/Scripts/myScriptBitbucket.js
+++ lym/Scripts/myScriptBitbucket.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         BitbucketReviewer
 // @namespace    http://tampermonkey.net/
-// @version      3.2
+// @version      3.3
 // @description  try to take over the world!
 // @author       You
 // @match        http://suus0003.w10:7990/projects/cnb/repos/*
