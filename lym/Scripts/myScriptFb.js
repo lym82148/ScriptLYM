6d1788924141f240f69205aad258c2e018773b22
LiuYiming <yiming.sh.liu@partner.bmw.com>
2019-08-29 17:43:48 +0800

    fb

--- lym/Scripts/myScriptFb.js
+++ lym/Scripts/myScriptFb.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         FbDeleteFriends
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.facebook.com/profile.php*
@@ -16,20 +16,36 @@
                 clearInterval(tkes);
                 var count = 0;
                 for (var i = 0; i < document.getElementsByClassName('_39g5').length; i++) {
+                    var tmp = document.getElementsByClassName('_39g5')[i];
+                    do {
+                        tmp = tmp.parentNode;
+                    } while (tmp.tagName != 'LI');
+                    tmp.seen = true;
                     if (document.getElementsByClassName('_39g5')[i].text.indexOf(',') >= 0) {
-                        window.open(document.getElementsByClassName('_39g5')[i].previousElementSibling.children[0].href)
+                        //console.log(document.getElementsByClassName('_39g5')[i].previousElementSibling.children[0].href)
                         count++;
                         if (count > 100) {
                             return;
                         }
+                    } else {
+                        console.log(document.getElementsByClassName('_39g5')[i])
+                        tmp.style.display = 'none';
+                    }
+
+                }
+                for (i = 0; i < document.getElementsByClassName('_698').length; i++) {
+                    if (!document.getElementsByClassName('_698')[i].seen) {
+                        document.getElementsByClassName('_698')[i].style.display = 'none';
                     }
 
                 }
             }
         }, 100);
     };
+
     var k = document.createElement('button');
-    k.innerText = '查找待删好友';
+    k.style.margin = '5px'
+    k.innerText = '过滤好友';
     k.onclick = cl;
     document.getElementById('u_0_a').append(k);
 
