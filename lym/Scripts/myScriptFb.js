3515cebd4a9d6f843ad2a0cd44538dece9fe0aea
LiuYiming <yiming.sh.liu@partner.bmw.com>
2019-08-29 16:52:28 +0800

    add fb

---
+++ lym/Scripts/myScriptFb.js
@@ -0,0 +1,39 @@
+﻿// ==UserScript==
+// @name         FbDeleteFriends
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.facebook.com/profile.php*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    var cl = function () {
+        tkes = setInterval(() => {
+            document.scrollingElement.scrollTop += 1000;
+            if (document.getElementById('medley_header_movies') != null) {
+                clearInterval(tkes);
+                var count = 0;
+                for (var i = 0; i < document.getElementsByClassName('_39g5').length; i++) {
+                    if (document.getElementsByClassName('_39g5')[i].text.indexOf(',') >= 0) {
+                        window.open(document.getElementsByClassName('_39g5')[i].previousElementSibling.children[0].href)
+                        count++;
+                        if (count > 100) {
+                            return;
+                        }
+                    }
+
+                }
+            }
+        }, 100);
+    };
+    var k = document.createElement('button');
+    k.innerText = '查找待删好友';
+    k.onclick = cl;
+    document.getElementById('u_0_a').append(k);
+
+
+})();
+
+
