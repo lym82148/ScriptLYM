ccd27deb014d9a21745f7508006aad680e8f4d46
LiuYiming <yiming.sh.liu@partner.bmw.com>
2019-10-22 11:02:08 +0800

    mail change

--- lym/Scripts/myScriptOpsFilter.js
+++ lym/Scripts/myScriptOpsFilter.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         OpsFilter
 // @namespace    http://tampermonkey.net/
-// @version      5.2
+// @version      5.3
 // @description  try to take over the world!
 // @author       You
 // @match        xxxhttps://omcops.bmw.com.cn/Configuration/DeployConfiguration/NewChange*
@@ -667,10 +667,12 @@
         var releaseEnv = $('.nav.nav-tabs>.active>a').text();
         var title = 'deploy ' + serviceName + ' to ' + releaseEnv;
         var cc = 'DL-bmwconnected-bumper <bmwconnected-bumper@list.bmw.com>';
-        var mailto = 'Zhang Lei<Lei.SH.Zhang@partner.bmw.com>; Yuan Kai, (Kai.Yuan@partner.bmw.com);';
+        var mailto = 'Chen Xiaojun, (Xiaojun.Chen@partner.bmw.com); Yuan Kai, (Kai.Yuan@partner.bmw.com);';
         var omc = 'omc.cn.support <omc.cn.support@bmwgroup.com>; ';
-        var content = 'Hi All\r\n\r\n' + serviceName + ' 申请发' + releaseEnv + ' 环境，需要ops上approve。\r\n\r\nhttps://omcops.bmw.com.cn/Operation/Release/ReleaseManagement'  + '\r\n\r\nBuild No: ' + buildNo + '\r\n\r\n\r\n\r\n';
-        var omcContent = 'Hi All \r\n\r\n' + serviceName + ' 申请发' + releaseEnv + ' 环境，需要ops上approve。\r\n\r\nhttps://omcops.bmw.com.cn/Operation/Release/ReleaseManagement'  + '\r\n\r\nBuild No: ' + buildNo + '\r\n\r\n\r\n\r\n';
+        //var content = 'Hi All\r\n\r\n' + serviceName + ' 申请发' + releaseEnv + ' 环境，需要ops上approve。\r\n\r\nhttps://omcops.bmw.com.cn/Operation/Release/ReleaseManagement'  + '\r\n\r\nBuild No: ' + buildNo + '\r\n\r\n\r\n\r\n';
+        var content = 'Hi All\r\n\r\n\r\n\r\n';
+        //var omcContent = 'Hi All \r\n\r\n' + serviceName + ' 申请发' + releaseEnv + ' 环境，需要ops上approve。\r\n\r\nhttps://omcops.bmw.com.cn/Operation/Release/ReleaseManagement'  + '\r\n\r\nBuild No: ' + buildNo + '\r\n\r\n\r\n\r\n';
+        var omcContent = 'Hi All \r\n\r\n\r\n\r\n';
         mailto = encodeURIComponent(mailto);
         omc = encodeURIComponent(omc);
         content = encodeURIComponent(content);
@@ -679,7 +681,7 @@
         cc = encodeURIComponent(cc);
         console.log(content);
         if (type == "noconfig" || $(this).data('res') == 'no') {
-            configRecord = '无';
+            configRecord = '';
             if (releaseEnv == 'Prod') {
                 mailTo.href = 'mailto:' + mailto + omc + '&cc=' + cc + '&subject=' + title + '&body=' + content;
             } else {
@@ -687,6 +689,7 @@
             }
         }
         else {
+            configRecord = ' 有配置需要 opsTeam Approve：'+configRecord;
             mailTo.href = 'mailto:' + mailto + omc + '&cc=' + cc + '&subject=' + title + '&body=' + omcContent;
         }
         console.log(jsonData)
@@ -711,7 +714,7 @@
         $('body').children().removeClass('hideForCopy');
         return true;
     };
-    var configRecord = '无';
+    var configRecord = '';
     var copy = function (jsonData) {
         if(!jsonData){
             return;
@@ -719,6 +722,7 @@
         var data = {};
         data.serviceName = jsonData.Service || '';
         data.userName = '';
+        data.buildNo=jsonData.JenkinsBuildNo || '';
         data.releaseEnv = $('.nav.nav-tabs>.active>a').text() || '';
         data.releaseNote = jsonData.ReleaseNotes.split('\n').map(function(a,b){if(a){return a.trim();}}).filter(function(a){return a;}).join('<br/>');
         //releaseNote=releaseNote.split('\n').map(function(a,b){if(a){return a.trim();}}).filter(function(a){return a;}).join('\r\n');
@@ -932,130 +936,18 @@
         hrefFun();
     }
     /*jshint multistr:true */
-    var model = '<div>\
-\
-<p class="MsoNormal"><span lang="EN-US">&nbsp;</span></p>\
-\
-<table class="MsoNormalTable" border="0" cellspacing="0" cellpadding="0" width="1023" style="width: 767.25pt; margin-left: 37.85pt;">\
-<tbody><tr style="mso-yfti-irow:0;mso-yfti-firstrow:yes;height:15.0pt">\
-<td width="192" nowrap="" style="width:144.0pt;border:solid windowtext 1.0pt;\
-background:#9BC2E6;padding:0cm 5.4pt 0cm 5.4pt;height:15.0pt">\
-<p class="MsoNormal"><span style="font-family:宋体">部署应用</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-<td width="831" nowrap="" valign="bottom" style="width:623.25pt;border:solid windowtext 1.0pt;\
-border-left:none;padding:0cm 5.4pt 0cm 5.4pt;height:15.0pt">\
-<p class="MsoNormal"><span lang="EN-US"><a href="{{url}}"><span style="font-size: 10.5pt; font-family: Helvetica, sans-serif; color: rgb(66, 139, 202); background-image: initial; background-position: initial; background-size: initial; background-repeat: initial; background-attachment: initial; background-origin: initial; background-clip: initial;">{{serviceName}}</span></a><o:p></o:p></span></p>\
-</td>\
-</tr>\
-<tr style="mso-yfti-irow:1;height:15.0pt">\
-<td width="192" nowrap="" style="width:144.0pt;border:solid windowtext 1.0pt;\
-border-top:none;background:#9BC2E6;padding:0cm 5.4pt 0cm 5.4pt;height:15.0pt">\
-<p class="MsoNormal"><span style="font-family:宋体">部署环境</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-<td width="831" nowrap="" valign="bottom" style="width:623.25pt;border-top:none;\
-border-left:none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;\
-padding:0cm 5.4pt 0cm 5.4pt;height:15.0pt">\
-<p class="MsoNormal"><span lang="EN-US">{{releaseEnv}}<o:p></o:p></span></p>\
-</td>\
-</tr>\
-<tr style="mso-yfti-irow:2;height:17.0pt">\
-<td width="192" nowrap="" style="width:144.0pt;border:solid windowtext 1.0pt;\
-border-top:none;background:#9BC2E6;padding:0cm 5.4pt 0cm 5.4pt;height:17.0pt">\
-<p class="MsoNormal"><span style="font-family:宋体">部署内容</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-<td width="831" valign="bottom" style="width:623.25pt;border-top:none;border-left:\
-none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;\
-padding:0cm 5.4pt 0cm 5.4pt;height:17.0pt">\
-<p class="MsoNormal"><span lang="EN-US">{{releaseNote}}<o:p></o:p></span></p>\
-</td>\
-</tr>\
-<tr style="mso-yfti-irow:3;height:15.0pt">\
-<td width="192" nowrap="" style="width:144.0pt;border:solid windowtext 1.0pt;\
-border-top:none;background:#9BC2E6;padding:0cm 5.4pt 0cm 5.4pt;height:15.0pt">\
-<p class="MsoNormal"><span style="font-family:宋体">部署包地址</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-<td width="831" nowrap="" valign="bottom" style="width:623.25pt;border-top:none;\
-border-left:none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;\
-padding:0cm 5.4pt 0cm 5.4pt;height:15.0pt"></td>\
-</tr>\
-<tr style="mso-yfti-irow:4;height:15.65pt">\
-<td width="192" nowrap="" style="width:144.0pt;border:solid windowtext 1.0pt;\
-border-top:none;background:#9BC2E6;padding:0cm 5.4pt 0cm 5.4pt;height:15.65pt">\
-<p class="MsoNormal"><span lang="EN-US" style="font-family:宋体">Tag</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-<td width="831" valign="bottom" style="width:623.25pt;border-top:none;border-left:\
-none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;\
-padding:0cm 5.4pt 0cm 5.4pt;height:15.65pt">\
-<p class="MsoNormal"><span lang="EN-US" style="font-size: 10.5pt; font-family: Helvetica, sans-serif; color: rgb(31, 73, 125); background-image: initial; background-position: initial; background-size: initial; background-repeat: initial; background-attachment: initial; background-origin: initial; background-clip: initial;">{{tagName}}</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-</tr>\
-<tr style="mso-yfti-irow:5;height:7.1pt">\
-<td width="192" nowrap="" style="width:144.0pt;border:solid windowtext 1.0pt;\
-border-top:none;background:#9BC2E6;padding:0cm 5.4pt 0cm 5.4pt;height:7.1pt">\
-<p class="MsoNormal"><span style="font-family:宋体">配置变更</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-<td width="831" valign="bottom" style="width:623.25pt;border-top:none;border-left:\
-none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;\
-padding:0cm 5.4pt 0cm 5.4pt;height:7.1pt">\
-<p class="MsoNormal"><span style="font-family:宋体;color:#1F497D">{{config}}</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-</tr>\
-<tr style="mso-yfti-irow:6;height:17.9pt">\
-<td width="192" nowrap="" style="width:144.0pt;border:solid windowtext 1.0pt;\
-border-top:none;background:#9BC2E6;padding:0cm 5.4pt 0cm 5.4pt;height:17.9pt">\
-<p class="MsoNormal"><span style="font-family:宋体">数据库变更</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-<td width="831" valign="bottom" style="width:623.25pt;border-top:none;border-left:\
-none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;\
-padding:0cm 5.4pt 0cm 5.4pt;height:17.9pt">\
-<p class="MsoNormal"><span style="font-family:宋体;color:#1F497D">无</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-</tr>\
-<tr style="mso-yfti-irow:7;height:4.85pt">\
-<td width="192" nowrap="" style="width:144.0pt;border:solid windowtext 1.0pt;\
-border-top:none;background:#9BC2E6;padding:0cm 5.4pt 0cm 5.4pt;height:4.85pt">\
-<p class="MsoNormal"><span style="font-family:宋体">其他组件</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-<td width="831" valign="bottom" style="width:623.25pt;border-top:none;border-left:\
-none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;\
-padding:0cm 5.4pt 0cm 5.4pt;height:4.85pt">\
-<p class="MsoNormal"><span style="font-family:宋体;color:#1F497D">无</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-</tr>\
-<tr style="mso-yfti-irow:8;height:15.0pt">\
-<td width="192" nowrap="" style="width:144.0pt;border:solid windowtext 1.0pt;\
-border-top:none;background:#9BC2E6;padding:0cm 5.4pt 0cm 5.4pt;height:15.0pt">\
-<p class="MsoNormal"><span lang="EN-US">INT</span><span style="font-family:宋体">计划部署时间</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-<td width="831" nowrap="" valign="bottom" style="width:623.25pt;border-top:none;\
-border-left:none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;\
-padding:0cm 5.4pt 0cm 5.4pt;height:15.0pt">\
-<p class="MsoNormal"><span style="font-family:宋体;color:#1F497D">无</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-</tr>\
-<tr style="mso-yfti-irow:9;height:15.0pt">\
-<td width="192" nowrap="" style="width:144.0pt;border:solid windowtext 1.0pt;\
-border-top:none;background:#9BC2E6;padding:0cm 5.4pt 0cm 5.4pt;height:15.0pt">\
-<p class="MsoNormal"><span lang="EN-US">Prod</span><span style="font-family:宋体">计划部署时间</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-<td width="831" nowrap="" valign="bottom" style="width:623.25pt;border-top:none;\
-border-left:none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;\
-padding:0cm 5.4pt 0cm 5.4pt;height:15.0pt">\
-<p class="MsoNormal"><span style="font-family:宋体;color:#1F497D">无</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-</tr>\
-<tr style="mso-yfti-irow:10;mso-yfti-lastrow:yes;height:15.0pt">\
-<td width="192" nowrap="" style="width:144.0pt;border:solid windowtext 1.0pt;\
-border-top:none;background:#9BC2E6;padding:0cm 5.4pt 0cm 5.4pt;height:15.0pt">\
-<p class="MsoNormal"><span style="font-family:宋体">相关人员</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-<td width="831" nowrap="" valign="bottom" style="width:623.25pt;border-top:none;\
-border-left:none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;\
-padding:0cm 5.4pt 0cm 5.4pt;height:15.0pt">\
-<p class="MsoNormal"><span lang="EN-US" style="font-family:宋体;color:#1F497D">{{userName}}</span><span lang="EN-US"><o:p></o:p></span></p>\
-</td>\
-</tr>\
-</tbody></table>\
-\
-<p class="MsoNormal"><span lang="EN-US">&nbsp;</span></p></div>';
+    var model = '\
+&nbsp;\
+<style>td{border:1px solid;padding:4px}</style>\
+<table width="0" style="\
+    border-collapse: collapse;\
+" class=""><tbody><tr><td width="266"><div><p>Service name</p></div></td><td width="126"><div><p> Build No</p></div></td><td width="139"><div><p> Stg/Prod</p></div></td><td width="201"><div><p> Passed the test or not</p></div></td><td width="179"><div><p> Product owner （name）</p></div></td><td width="211"><div><p>QA （name）</p></div></td></tr><tr><td width="266" style="\
+"><div><p> {{serviceName}}</p></div></td><td width="126" style="\
+"><div><p style="\
+    border-right: none slo;\
+"> {{buildNo}}</p></div></td><td width="139"><div><p> {{releaseEnv}}</p></div></td><td width="201"><div><p></p></div></td><td width="179"><div><p> YuanKai</p></div></td><td width="211"><div><p> ZhangXiaoqi</p></div></td></tr></tbody></table>\
+&nbsp;\
+<p class="MsoNormal"><span style="font-family:宋体;color:#ff4e4e">{{config}}</span><span lang="EN-US"><o:p></o:p></span></p>\
+&nbsp;\
+';
 })();
