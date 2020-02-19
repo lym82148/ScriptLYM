c23be8087a063f57af2e28becc61c421cd7b71c8
iherb_liuyiming <yiming.liu@iherb.com>
2020-02-19 17:55:52 +0800

    test

---
+++ lym/Scripts2/test.js
@@ -0,0 +1,13 @@
+﻿// ==UserScript==
+// @name         OpsAutoEmail
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.baidu.com*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    console.log(3);
+})();
