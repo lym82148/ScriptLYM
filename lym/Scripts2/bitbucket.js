40311f5d230689c7d2e38ab38885d4538f573277
iherb_liuyiming <yiming.liu@iherb.com>
2020-03-11 15:52:44 +0800

    bitbucket

---
+++ lym/Scripts2/bitbucket.js
@@ -0,0 +1,35 @@
+﻿// ==UserScript==
+// @name         Test
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  try to take over the world!
+// @author       You
+// @match        https://bitbucket.org/*/pull-requests/new/*
+// @match        https://bitbucket.org/*/pull-requests/update/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    var curUserName = $('body').data('current-user').displayName;
+    while (true) {
+        await tamperMonkey.sleep(1000);
+        console.log(curUserName);
+    }
+    console.log(3);
+})();
+function getDefaultBranch(serviceName) {
+    var branchName = '';
+    switch (serviceName) {
+        case 'legacy.checkout-web':
+            branchName ='staging';
+            break;
+        default:
+            branchName = 'master';
+            break;
+    }
+    return branchName;
+};
+var defaultUserList = [
+    { userName: 'Terry Luo'},
+    { userName: 'Yiming Liu' }
+];
---
+++ lym/Scripts2/common.js
@@ -0,0 +1,18 @@
+﻿// ==UserScript==
+// @name         Test
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  try to take over the world!
+// @author       You
+// @match        *
+// @grant        none
+// ==/UserScript==
+
+var tamperMonkey = {};
+tamperMonkey.sleep = function (time) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve();
+        }, time);
+    });
+};
