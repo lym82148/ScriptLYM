87b1e8edd06b23ecd2ad1276cb2f15dfda153966
iherb_liuyiming <yiming.liu@iherb.com>
2020-03-16 12:12:08 +0800

    pull request approver、build link、deploy link

--- lym/Scripts2/bitbucket.js
+++ lym/Scripts2/bitbucket.js
@@ -1,36 +1,84 @@
 ﻿// ==UserScript==
 // @name         Bitbucket
 // @namespace    http://tampermonkey.net/
-// @version      2
-// @description  try to take over the world!
-// @author       You
-// @match        https://bitbucket.org/*/pull-requests/new/*
-// @match        https://bitbucket.org/*/pull-requests/update/*
+// @version      3
+// @description  pull request approver、build link、deploy link
+// @author       Yiming Liu
+// @match        https://bitbucket.org/*
 // @grant        none
 // ==/UserScript==
 
-(function () {
 
-    var curUserName = $('body').data('current-user').displayName;
-    while (true) {
-        await tamperMonkey.sleep(1000);
-        console.log(curUserName);
+(async function () {
+    // Service Name
+    var serviceName = await lymTM.async(() => $('div.css-1xaaz5m').html());
+    var bread = $(`ol.aui-nav-breadcrumbs>li>a:first,a[title="${serviceName}"]`).parent().siblings(':last');
+
+    // Build Link
+    var buildLink = $(lymTM.createLink('Build', lymTM.getBuildLink(serviceName))).css('margin-left', '20px');
+    bread.append(buildLink);
+
+    // Deploy Link
+    var deployLink = $(lymTM.createLink('Deploy', lymTM.getDeployLink(serviceName))).css('margin-left', '20px');
+    bread.append(deployLink);
+
+    // 不是创建分支页面
+    if (!$('#id_reviewers_group').length) {
+        return;
     }
-    console.log(3);
+
+    var curUserName = $('#bb-bootstrap').data('current-user').displayName;
+    console.table({ curUserName, serviceName });
+
+    var targetBranch = lymTM.getDefaultBranch(serviceName);
+    var chooseBranch = async function () {
+        // 选默认分支
+        $('#id_dest_group div.branch-field>a.select2-choice').mousedown();
+        await lymTM.async();
+        $('#select2-drop ul.select2-result-sub>li').filter((a, b) => { return b.textContent == targetBranch; }).mouseup();
+    }
+    await chooseBranch();
+    // 选择分支提示链接
+    $('div.branch-field:eq(1)').before(lymTM.createLinkButton('Choose ' + targetBranch, chooseBranch)).css('margin-top', '3px').closest('div.branch-field-container').css('margin-top', '3px');
+
+    // 审核者列表
+    var approveUsers = lymTM.getApproveUsers(curUserName);
+    // 选择审核者提示链接
+    var element = $('<div class="field-group"><label></label></div>');
+    element.append(lymTM.createLinkButton('Add  ' + approveUsers.map((a, b) => a.userName).join(' & '), () => tabFun({ key: 'Tab' })));
+    $('#id_reviewers_group').before(element);
+
+    // 预热搜索列表
+    await search(approveUsers, true);
+    // 隐藏搜索列表
+    $('#select2-drop-mask').click();
+    // 等待搜索结果加载准备
+    await lymTM.async(500);
+
+    var tabFun = async function (e) {
+        if (e.key == 'Tab') {
+            var curList = $('#id_reviewers_group ul.select2-choices>li.select2-search-choice').map((a, b) => b.textContent.trim()).toArray();
+            var secFilterUserList = approveUsers.filter((a) => !curList.includes(a.userName));
+            await search(secFilterUserList);
+        }
+    };
+    await tabFun({ key: 'Tab' });
+
 })();
-function getDefaultBranch(serviceName) {
-    var branchName = '';
-    switch (serviceName) {
-        case 'legacy.checkout-web':
-            branchName ='staging';
-            break;
-        default:
-            branchName = 'master';
-            break;
+
+var event = document.createEvent('HTMLEvents');
+// 事件类型，是否冒泡，是否阻止浏览器的默认行为
+event.initEvent("input", false, true);
+async function search(list, isWarmUp) {
+    for (var i in list) {
+        var input = $('#id_reviewers_group ul.select2-choices>li.select2-search-field>input').click();
+        console.log('search for username:' + list[i].userName);
+        input.val(list[i].userName).get(0).dispatchEvent(event);
+        await lymTM.async();
+        if (!isWarmUp) {
+            // 选择搜索结果的第一项
+            $('#select2-drop>ul>li').mouseup();
+        }
     }
-    return branchName;
-};
-var defaultUserList = [
-    { userName: 'Terry Luo'},
-    { userName: 'Yiming Liu' }
-];
+}
+
--- lym/Scripts2/common.js
+++ lym/Scripts2/common.js
@@ -1,19 +1,85 @@
 ﻿// ==UserScript==
 // @name         Common
 // @namespace    http://tampermonkey.net/
-// @version      2
-// @description  try to take over the world!
-// @author       You
-// @match        *
+// @version      3
+// @description  configs & util
+// @author       Yiming Liu
+// @include      *
 // @grant        none
 // ==/UserScript==
 
-var tamperMonkey = {};
+window.lymTM = {
+    sleep: function (time) {
+        return new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                resolve();
+            }, time);
+        });
+    },
+    async: async function (func, time = 100) {
+        if (typeof func == 'number') {// 参数是数字
+            await this.sleep(func);
+        }
+        else if (func) { // 参数是方法
+            var res;
+            while (!(res = func())) {
+                await this.sleep(time);
+            }
+            return res;
+        } else { // 没有参数
+            await this.sleep(0);
+        }
+    },
+    createLink: function (text, href) {
+        var link = document.createElement('a');
+        link.style.color = '#ff6e6e';
+        link.style.fontSize = '16px';
+        link.style.textDecoration = 'underline';
+        link.href = href;
+        link.innerHTML = text;
+        return link;
+    },
+    createLinkButton: function (text, func) {
+        var link = document.createElement('a');
+        link.style.color = '#ff6e6e';
+        link.style.fontSize = '16px';
+        link.style.display = 'block';
+        link.style.textDecoration = 'underline';
+        link.href = 'javascript:void(0);';
+        link.onclick = func;
+        link.innerHTML = text;
+        return link;
+    },
+    teamMembers: [
+        { "userName": "Terry (Xiaoyu) Luo" },
+        { "userName": "Yiming Liu" },
+        //         { "userName": "Tony (Sichao) Qian" },
+        //         { "userName": "Diri (Jianwei) Guo" },
 
-tamperMonkey.sleep = function (time) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            resolve();
-        }, time);
-    });
+    ],
+    serviceConfigs: [
+        {
+            "name": "legacy.checkout-web",
+            "fullslug": "iherbllc/legacy.checkout-web",
+            "defaultBranch": "staging",
+            "buildLink": "https://tfs.iherb.net/tfs/iHerb%20Projects%20Collection/iHerbDev/Orders%20and%20Communications/_build/index?context=mine&path=%5CWebDev&definitionId=1053&_a=completed",
+            "deployLink": "https://deploy.iherb.net/app#/projects/shop/overview",
+        },
+    ],
+    getDefaultBranch: function (name) {
+        var res = this.serviceConfigs.filter((a) => a.name == name);
+        return res.length ? res[0].defaultBranch : 'master';
+    },
+    getBuildLink: function (name) {
+        var res = this.serviceConfigs.filter((a) => a.name == name);
+        return res.length ? res[0].buildLink : 'javascript:alert("build link not found for service:' + name + '")';
+    },
+    getDeployLink: function (name) {
+        var res = this.serviceConfigs.filter((a) => a.name == name);
+        return res.length ? res[0].deployLink : 'javascript:alert("build link not found for service:' + name + '")';
+    },
+    getApproveUsers: function (curUser) {
+        return this.teamMembers.filter((a) => a.userName != curUser);
+    }
 };
+
