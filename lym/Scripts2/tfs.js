e4b3de7af2212d05c6b81d6e52e72b0bcfcfe972
iherb_liuyiming <yiming.liu@iherb.com>
2020-03-16 14:19:56 +0800

    tfs

---
+++ lym/Scripts2/tfs.js
@@ -0,0 +1,14 @@
+﻿// ==UserScript==
+// @name         Tfs
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  tfs
+// @author       Yiming Liu
+// @match        https://tfs.iherb.net/tfs/iHerb%20Projects%20Collection/iHerbDev/Orders%20and%20Communications/_build/index*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+
+    console.log(3);
+})();
