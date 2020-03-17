798fee9bc8f23c3ed8b91afc74e30ed9feaccbd3
iherb_liuyiming <yiming.liu@iherb.com>
2020-03-17 17:20:26 +0800

    octopus

---
+++ lym/Scripts2/octopus.js
@@ -0,0 +1,19 @@
+﻿// ==UserScript==
+// @name         Octopus
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  CD
+// @author       Yiming Liu
+// @match        https://deploy.iherb.net/app#/projects/shop/overview*
+// @grant        none
+// ==/UserScript==
+
+(async function wrap() {
+    var time = lymTM.start();
+    await process(wrap, time);
+    time.end();
+})();
+
+async function process(wrap, time) {
+    
+}
--- lym/Scripts2/tfs.js
+++ lym/Scripts2/tfs.js
@@ -1,14 +1,31 @@
 ﻿// ==UserScript==
 // @name         Tfs
 // @namespace    http://tampermonkey.net/
-// @version      1
-// @description  tfs
+// @version      2
+// @description  CI
 // @author       Yiming Liu
 // @match        https://tfs.iherb.net/tfs/iHerb%20Projects%20Collection/iHerbDev/Orders%20and%20Communications/_build/index*
 // @grant        none
 // ==/UserScript==
 
-(function () {
-
-    console.log(3);
+(async function wrap() {
+    var time = lymTM.start();
+    await process(wrap, time);
+    time.end();
 })();
+
+async function process(wrap, time) {
+    if (lymTM.getQueryString('definitionId') == 1053) {
+        var buildList = await lymTM.async($('div.summary-build-row span.build-status-column'));
+        // when DOM is changed, run script again
+        lymTM.nodeRemoveCallback(buildList, func);
+
+        buildList.before('<span style="display:table-cell"></span>');
+        buildList.filter(':has([title=succeeded])').each(
+            (a, b) => {
+                var buildId = $(b).siblings('.build-detail-link-column').text();
+                $(lymTM.createLink('deploy', `https://deploy.iherb.net/app#/projects/shop/overview#${buildId}`)).css({ 'font-size': '14px', 'margin-left': '4px' }).appendTo($(b).prev());
+            }
+        );
+    }
+}
