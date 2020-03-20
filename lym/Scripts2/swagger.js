5d3a455dbd65b964a016e159144c707f3589c713
iherb_liuyiming <yiming.liu@iherb.com>
2020-03-20 15:45:36 +0800

    swagger

---
+++ lym/Scripts2/swagger.js
@@ -0,0 +1,21 @@
+﻿// ==UserScript==
+// @name         Swagger
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  swagger
+// @author       Yiming Liu
+// @match        https://localhost:44300/swagger/*
+// ==/UserScript==
+
+(async function wrap() {
+    // force async at first
+    await lymTM.async();
+    var time = lymTM.start();
+    await process(wrap, time);
+    // log execution time
+    time.end();
+})();
+
+async function process(func, time) {
+  
+}
