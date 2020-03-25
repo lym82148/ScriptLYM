91838f35347fe0bda8818d0d80ddf61a0972f2fc
iherb_liuyiming <yiming.liu@iherb.com>
2020-03-25 15:21:50 +0800

    jenkins

---
+++ lym/Scripts2/jenkins.js
@@ -0,0 +1,22 @@
+﻿// ==UserScript==
+// @name         Jenkins
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  CI CD
+// @author       Yiming Liu
+// @match        https://jenkins-ci.iherb.net/*
+// @match        https://jenkins.iherb.io/*
+// @grant        none
+// ==/UserScript==
+
+(async function wrap() {
+    // force async at first
+    await lymTM.async();
+    var time = lymTM.start();
+    await process(wrap, time);
+    // log execution time
+    time.end();
+})();
+async function process(wrap, time) {
+
+}
